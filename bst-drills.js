8d5b09a6f6450339acef975d925d0b539d244350
Albert Sare <thesundaymailman@gmail.com>
2018-07-31 09:24:47 -0700

    implemented BinarySearchTree

--- bst-drills.js
+++ bst-drills.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const BinarySearchTree = require('./bst.js');
+
+const BST = new BinarySearchTree();
+
+const data = [3,1,4,6,9,2,5,7];
+data.forEach(each => BST.insert(each));
+
+console.log(BST);
